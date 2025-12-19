b1441f2f7a1e044fd73d7ce646280227714aa9cb
Sahiljoshi1606 <joshiom7387@gmail.com>
2025-12-19 21:52:42 +0530

    update

---
+++ DAY-1/Http_Module.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    var parseUrl = url.parse(req.url, true);
+    var parseName = parseUrl.query.name;
+    var parseBrand = parseUrl.query.brand;
+
+    if (parseUrl.pathname === '/') {
+        res.write("<h1>Home Page</h1>");
+        res.write("<p>Welcome to my Node.js Server</p>");
+    }
+    else if (parseUrl.pathname === '/about') {
+        res.write("<h1>About Page</h1>");
+        res.write("<p>This is the about page</p>");
+    }
+    else if (parseUrl.pathname === '/contact') {
+        res.write("<h1>Contact Page</h1>");
+        res.write("<p>Contact us at support@example.com</p>");
+    }
+    else if (parseUrl.pathname === '/product') {
+        res.write("<h1>Product Page</h1>");
+        res.write(`<p>You are searching for <b>${parseName}</b> product of <b>${parseBrand}</b> brand</p>`);
+    }
+    else {
+        res.write("<h1>404 Page Not Found</h1>");
+    }
+
+    res.end();
+}).listen(9999);
+
+console.log("Server running on port 9999");
