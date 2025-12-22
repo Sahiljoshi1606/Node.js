ddba975c6079dd8b8cf006414c95539e53f6afcd
Sahiljoshi1606 <joshiom7387@gmail.com>
2025-12-22 11:20:14 +0530

    update

--- DAY-1/Http_Module.js
+++ DAY-1/Http_Module.js
@@ -2,7 +2,7 @@ var http = require('http');
 var url = require('url');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200, {  'Content-Type': 'text/html'});
 
     var parseUrl = url.parse(req.url, true);
     var parseName = parseUrl.query.name;
@@ -29,6 +29,6 @@ http.createServer(function (req, res) {
     }
 
     res.end();
-}).listen(9999);
+}).listen(8888);
 
 console.log("Server running on port 9999");
